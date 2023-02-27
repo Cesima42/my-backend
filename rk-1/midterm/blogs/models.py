c5cb4de113b722e74b2bea9632fb2f39807aa004
Adil Taileken <taileken05@gmail.com>
2023-02-27 12:43:28 +0600

    ...
    ...

--- rk-1/midterm/blogs/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Blog(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.CharField(max_length=1000)
-    owner = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.title
